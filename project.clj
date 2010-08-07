ff6824559301fb5599311a940e3fbd65d09bceeb
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-07 14:21:41 -0700

    0.1.0-RC1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-time "0.1.0-SNAPSHOT"
+(defproject clj-time "0.1.0-RC1"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dependencies [[org.clojure/clojure "1.2.0-RC2"]
                  [org.clojure/clojure-contrib "1.2.0-RC2"]
