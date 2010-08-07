f3514e0ab927b6b9969d8bc1fffaffb418ef501c
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-07 14:17:18 -0700

    update dependencies for clojure 1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clj-time "0.1.0-SNAPSHOT"
   :description "A date and time library for Clojure, wrapping Joda Time."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [joda-time "1.6"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0-RC2"]
+                 [org.clojure/clojure-contrib "1.2.0-RC2"]
+                 [joda-time "1.6"]])
