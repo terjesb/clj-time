2816f84156c12e96ee175b063c8927db72a1f2ea
Matt Revelle <mrevelle@gmail.com>
2010-12-31 00:11:47 -0500

    Incremented version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-time "0.2.0-SNAPSHOT"
+(defproject clj-time "0.3.0-SNAPSHOT"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
