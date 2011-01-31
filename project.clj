b2237e5e3bfc3649fd702b30485a4b83c013fdae
Matt Revelle <mrevelle@gmail.com>
2011-01-30 22:04:29 -0800

    Woven repos.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [joda-time "1.6"]])
+                 [joda-time "1.6"]]
+  :repositories {"snapshots" "http://mvn.getwoven.com/repos/woven-public-snapshots"
+                 "releases" "http://mvn.getwoven.com/repos/woven-public-releases"})
