20e8cbc9b9c430f70278c30c7e89772be7b79220
Sean Corfield <sean@corfield.org>
2011-10-31 17:27:41 -0700

    Bump to 0.3.2 for Clojure 1.3.0 compatibility and new Period-based in-* functions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-time "0.3.2-SNAPSHOT"
+(defproject clj-time "0.3.2"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [joda-time "2.0"]])
