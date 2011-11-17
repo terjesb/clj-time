7c9ca0a3ac038a12504991516c0820a54a1f8509
Sean Corfield <sean@corfield.org>
2011-11-16 17:36:35 -0800

    Snapshot for 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-time "0.3.2"
+(defproject clj-time "0.3.3-SNAPSHOT"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dev-dependencies [[utilize "0.1.2"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
