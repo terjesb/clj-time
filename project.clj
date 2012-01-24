403bed9547b970c8a6606a5b7771f7fdb16b1be2
Ben Mabey <ben@benmabey.com>
2012-01-24 16:27:15 -0700

    introduces lein-multi to test for 1.3 compatibility
    To use run `lein plugin install lein-multi 1.1.0`
    and then `lein multi test`

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject clj-time "0.3.4"
+(defproject clj-time "0.3.5-SNAPSHOT"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dev-dependencies [[utilize "0.1.2"]]
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joda-time "2.0"]])
+  :dependencies [[joda-time "2.0"]]
+  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]]
+               "1.2" [[org.clojure/clojure "1.2.1"]]})
