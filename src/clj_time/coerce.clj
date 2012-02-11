d111faeb469b5f0db8b1f2d4aadd85ee536403bc
Roman <roman@burningswell.com>
2012-02-11 12:48:17 +0100

    Added the ICoerce protocol and implemented to-long, to-date and to-string.

--- src/clj_time/coerce.clj
+++ src/clj_time/coerce.clj
@@ -1,10 +1,10 @@
 (ns clj-time.coerce
   "Utilites to coerce Joda DateTime instances to and from various other types.
    For example, to convert a Joda DateTime to and from a Java long:
-   
+
      => (to-long (date-time 1998 4 25))
      893462400000
-     
+
      => (from-long 893462400000)
      #<DateTime 1998-04-25T00:00:00.000Z>"
   (:refer-clojure :exclude [extend])
@@ -13,17 +13,20 @@
   (:import (org.joda.time DateTime DateTimeZone))
   (:import java.util.Date))
 
-(defn to-long
-  "Returns the number of milliseconds that the given DateTime is after Unix
-   epoch."
-  [#^DateTime dt]
-  (.getMillis dt))
+(defprotocol ICoerce
+  (to-long [obj]
+    "Returns the number of milliseconds the given obj is after the Unix epoch.")
+  (to-date [obj]
+    "Returns a Java Date object corresponding to the given obj.")
+  (to-string [obj]
+    "Returns a string representation of obj in UTC time-zone
+    using (ISODateTimeFormat/dateTime) date-time representation."))
 
 (defn from-long
   "Returns a DateTime instance in the UTC time zone corresponding to the given
    number of milliseconds after the Unix epoch."
-   [#^Long millis]
-   (DateTime. millis #^DateTimeZone utc))
+  [#^Long millis]
+  (DateTime. millis #^DateTimeZone utc))
 
 (defn from-string
   "return DateTime instance from string using
@@ -31,14 +34,9 @@
    which parses"
   [s]
   (first
-       (for [f (vals time-fmt/formatters)
-             :let [d (try (time-fmt/parse f s) (catch Exception _ nil))]
-             :when d] d)))
-
-(defn to-date
-  "Returns a Java Date object corresponding to the given DateTime instance."
-  [#^DateTime dt]
-  (Date. #^Long (to-long dt)))
+   (for [f (vals time-fmt/formatters)
+         :let [d (try (time-fmt/parse f s) (catch Exception _ nil))]
+         :when d] d)))
 
 (defn from-date
   "Returns a DateTime instance in the UTC time zone corresponding to the given
@@ -46,8 +44,11 @@
   [#^Date date]
   (from-long (.getTime date)))
 
-(defn to-string
-  "Returns a string representation of date in UTC time-zone using
-   (ISODateTimeFormat/dateTime) date-time representation. "
-  [#^DateTime dt]
-  (time-fmt/unparse (:date-time time-fmt/formatters) dt))
+(extend-type DateTime
+  ICoerce
+  (to-date [date-time]
+    (Date. (.getMillis date-time)))
+  (to-long [date-time]
+    (.getMillis date-time))
+  (to-string [date-time]
+    (time-fmt/unparse (:date-time time-fmt/formatters) date-time)))
