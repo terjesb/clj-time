224b4a6debd24792ca808c639169680e0daf459d
Roman <roman@burningswell.com>
2012-02-11 13:11:05 +0100

    Removed ICoerce implementaion for java.sql.Timestamp, since Timestamp is a subclass of java.util.Date.

--- src/clj_time/coerce.clj
+++ src/clj_time/coerce.clj
@@ -11,7 +11,7 @@
   (:use clj-time.core)
   (:require [clj-time.format :as time-fmt])
   (:import (org.joda.time DateTime DateTimeZone))
-  (:import java.util.Date java.sql.Timestamp))
+  (:import java.util.Date))
 
 (defprotocol ICoerce
   (to-long [obj]
@@ -74,14 +74,3 @@
     (.getMillis dt))
   (to-string [dt]
     (time-fmt/unparse (:date-time time-fmt/formatters) dt)))
-
-(extend-type Timestamp
-  ICoerce
-  (to-date [ts]
-    (Date. (to-long ts)))
-  (to-date-time [ts]
-    (DateTime. (to-long ts) utc))
-  (to-long [ts]
-    (.getTime ts))
-  (to-string [ts]
-    (to-string (to-date-time ts))))
