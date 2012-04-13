8ed1eefdca4334f4a1be029db68695e0bf950452
Sean Corfield <sean@corfield.org>
2012-04-12 19:23:57 -0700

    Bump to 0.3.8 for a release to include local time namespace.
    Update multi-deps to test against 1.4.0-beta7 as well.

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
 (def deps '[[joda-time "2.0"]])
 
 ;; project definition with additional dependencies:
-(defproject clj-time "0.3.8-SNAPSHOT"
+(defproject clj-time "0.3.8"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dev-dependencies [[utilize "0.1.2"]
                      [lein-multi "1.1.0"]]
   :dependencies ~(conj deps '[org.clojure/clojure "1.2.1"])
   :multi-deps {"1.3" ~(conj deps '[org.clojure/clojure "1.3.0"])
-               "1.4B" ~(conj deps '[org.clojure/clojure "1.4.0-beta1"])})
+               "1.4B" ~(conj deps '[org.clojure/clojure "1.4.0-beta7"])})
