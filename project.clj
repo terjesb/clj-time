95e23035af55fbaac448c5e2b22ad67090666e15
Sean Corfield <sean@corfield.org>
2012-04-15 16:34:51 -0700

    Clojure 1.2.1 => 1.3.0 (now 1.4.0 is out). Multi test on 1.2.1, 1.3.0, 1.4.0 and 1.5.0-master-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dev-dependencies [[utilize "0.1.2"]
                      [lein-multi "1.1.0"]]
-  :dependencies ~(conj deps '[org.clojure/clojure "1.2.1"])
-  :multi-deps {"1.3" ~(conj deps '[org.clojure/clojure "1.3.0"])
-               "1.4B" ~(conj deps '[org.clojure/clojure "1.4.0-beta7"])})
+  :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+  :dependencies ~(conj deps '[org.clojure/clojure "1.3.0"])
+  :multi-deps {"1.2"  ~(conj deps '[org.clojure/clojure "1.2.1"])
+               "1.4"  ~(conj deps '[org.clojure/clojure "1.4.0"])
+               "1.5S" ~(conj deps '[org.clojure/clojure "1.5.0-master-SNAPSHOT"])})
