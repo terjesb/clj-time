63af49d96c9bdbe96fdf07d6d3f6593bcc92c7e7
Sean Corfield <sean@corfield.org>
2012-04-19 10:02:58 -0700

    Release 0.4.1.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (def deps '[[joda-time "2.0"]])
 
 ;; project definition with additional dependencies:
-(defproject clj-time "0.4.1-SNAPSHOT"
+(defproject clj-time "0.4.1"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dev-dependencies [[utilize "0.1.2"]
                      [lein-multi "1.1.0"]]
