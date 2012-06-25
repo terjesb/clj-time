81dfb3bd438a3baa0e5b536868ea516708c50c55
Sean Corfield <sean@corfield.org>
2012-06-25 11:21:06 -0700

    Update to lein2

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-;; common dependencies:
-(def deps '[[joda-time "2.1"]])
-
-;; project definition with additional dependencies:
-(defproject clj-time "0.4.4-SNAPSHOT"
+(defproject clj-time/clj-time "0.4.4-SNAPSHOT" 
   :description "A date and time library for Clojure, wrapping Joda Time."
-  :dev-dependencies [[utilize "0.1.2"]
-                     [lein-multi "1.1.0"]]
-  :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"}
-  :dependencies ~(conj deps '[org.clojure/clojure "1.3.0"])
-  :multi-deps {"1.2"  ~(conj deps '[org.clojure/clojure "1.2.1"])
-               "1.4"  ~(conj deps '[org.clojure/clojure "1.4.0"])
-               "1.5A" ~(conj deps '[org.clojure/clojure "1.5.0-alpha2"])})
+  :url "https://github.com/seancorfield/clj-time"
+  :mailing-list {:name "clj-time mailing list"
+                 :archive "https://groups.google.com/forum/?fromgroups#!forum/clj-time"
+                 :post "clj-time@googlegroups.com"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :dependencies [[joda-time "2.1"] [org.clojure/clojure "1.3.0"]]
+  :min-lein-version "2.0.0"
+  :profiles {:dev  {:dependencies [[utilize "0.1.2"]]}
+             :1.2  {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5A {:dependencies [[org.clojure/clojure "1.5.0-alpha2"]]}}
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.4,default:dev,1.5A,default" "test"]})
