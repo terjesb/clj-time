c7c054b99bde42a33d60df522ee538e3c124e9b3
Sean Corfield <sean@corfield.org>
2012-09-12 19:26:28 -0700

    Change base version to 1.4.0; move 1.3.0 to multi-version test.
    Also move Sonatype snapshots repo to the only profile that uses it.

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :dependencies [[joda-time "2.1"] [org.clojure/clojure "1.3.0"]]
+  :dependencies [[joda-time "2.1"] [org.clojure/clojure "1.4.0"]]
   :min-lein-version "2.0.0"
-  :repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :profiles {:dev {:dependencies [[utilize "0.1.2"]]
                    :plugins [[codox "0.6.1"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
-  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.4,default:dev,1.5,default" "test"]})
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+                   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.2,default:dev,1.3,default:dev,1.5,default" "test"]})
