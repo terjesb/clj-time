f96262036a53bfaee8846439370131333b49711f
Sean Corfield <sean@corfield.org>
2013-08-18 15:35:35 -0700

    Start 0.6.1 dev stream

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-time/clj-time "0.6.0"
+(defproject clj-time/clj-time "0.6.1-SNAPSHOT"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :url "https://github.com/clj-time/clj-time"
   :mailing-list {:name "clj-time mailing list"
