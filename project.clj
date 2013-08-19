9c97dfdd8af77618174c64c5107afb9e26029d3d
Michael Klishin <michael@defprotocol.org>
2013-08-20 02:05:19 +0400

    Upgrade Joda Time to 2.3, fixes #81

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :dependencies [[joda-time "2.2"] [org.clojure/clojure "1.5.1"]]
+  :dependencies [[joda-time "2.3"] [org.clojure/clojure "1.5.1"]]
   :min-lein-version "2.0.0"
   :profiles {:dev {:plugins [[codox "0.6.1"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
