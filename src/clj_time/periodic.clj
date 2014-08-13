e23cbf1609d339b24af2a0c1960809927cbebcb5
siscia <simone@mweb.biz>
2014-08-13 10:29:32 +0200

    remove useless require

--- src/clj_time/periodic.clj
+++ src/clj_time/periodic.clj
@@ -1,12 +1,11 @@
 (ns clj-time.periodic
-  (:require [clj-time.internal.fn :as ifns]
-            [clj-time.core :as ct])
+  (:require [clj-time.core :as ct])
   (:import [org.joda.time DateTime ReadablePeriod Period]))
 
 (defn periodic-seq
   "Returns an infinite sequence of date-time values growing over specific period"
   [^DateTime start ^ReadablePeriod period-like]
-  (let [^Period period (.toPeriod period-like)]
-    (map (fn [^long i]
-         (ct/plus start (.multipliedBy period i)))
-       (iterate inc 0))))
+   (let [^Period period (.toPeriod period-like)]
+     (map (fn [^long i]
+            (ct/plus start (.multipliedBy period i)))
+          (iterate inc 0))))
