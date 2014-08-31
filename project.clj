f90725dc87b22a1b8781383364aa9bf6d2d41b77
Michael Klishin <michael@clojurewerkz.org>
2014-08-31 19:18:51 +0400

    0.9.0-beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-time/clj-time "0.9.0-SNAPSHOT"
+(defproject clj-time/clj-time "0.9.0-beta1"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :url "https://github.com/clj-time/clj-time"
   :mailing-list {:name "clj-time mailing list"
