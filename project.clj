74fed81a6f993084f288ad44555ed7c355391bbf
Michael Klishin <michael@clojurewerkz.org>
2014-12-25 17:57:22 +0300

    Bump Clojure 1.7

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                      :plugins      [[lein-midje "3.0.0"]
                                     [midje-readme "1.0.3"]]
                      :midje-readme {:require "[clj-time.core :as t] [clj-time.predicates :as pr]"}}
-             :1.7    {:dependencies [[org.clojure/clojure "1.7.0-alpha1"]]}
+             :1.7    {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
                    :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :aliases {"test-all" ["with-profile" "dev,master,default,midje:dev,default,midje:dev,1.7,midje" "test"]})
