f393378914c07299ced7547181d9ef7caed53a0a
Michael Klishin <michael@clojurewerkz.org>
2015-08-13 22:44:43 +0300

    Depend on Clojure 1.7.0

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
   :dependencies [[joda-time "2.8.2"]
-                 [org.clojure/clojure "1.6.0"]]
+                 [org.clojure/clojure "1.7.0"]]
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[org.clojure/java.jdbc "0.3.6"]]
                    :plugins [[codox "0.8.10"]]}
@@ -16,7 +16,7 @@
                      :plugins      [[lein-midje "3.1.3"]
                                     [midje-readme "1.0.3"]]
                      :midje-readme {:require "[clj-time.core :as t] [clj-time.predicates :as pr]"}}
-             :1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :master {:repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-                   :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
-  :aliases {"test-all" ["with-profile" "dev,master,default,midje:dev,default,midje:dev,1.7,midje" "test"]})
+                      :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
+  :aliases {"test-all" ["with-profile" "dev,master,default,midje:dev,default,midje:dev,1.6,midje" "test"]})
