9ec56e7baf22d24b988939b7ff7ec080c2eda7f6
David "novalis" Turner <novalis@novalis.org>
2018-10-09 17:25:20 -0400

    upgrade joda-time (#254)

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :dependencies [[joda-time "2.9.9"]
+  :dependencies [[joda-time "2.10"]
                  [org.clojure/clojure "1.9.0" :scope "provided"]]
   :min-lein-version "2.0.0"
   :global-vars {*warn-on-reflection* true}
