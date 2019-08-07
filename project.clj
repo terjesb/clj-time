60a840542ab0ca53ee2067416714793ed721ce21
Sean Corfield <sean@corfield.org>
2019-08-07 10:59:21 -0700

    Start 0.15.3 (snapshot) cycle

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def java7? (.startsWith (System/getProperty "java.version") "1.7"))
 
-(defproject clj-time/clj-time "0.15.2"
+(defproject clj-time/clj-time "0.15.3-SNAPSHOT"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :url "https://github.com/clj-time/clj-time"
   :mailing-list {:name "clj-time mailing list"
